3538c2e4f18c44875970bce05a26a40540305760
Mark Aigbe <b00118087@mytudublin.ie>
2021-04-07 13:26:19 +0100

    set up links for public to access sitemap, about and index

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,12 +8,34 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+    // adding page routes for public viewing
+
     /**
      * @Route("/", name="default")
      */
     public function index(): Response
     {
         $template = 'default/index.html.twig';
+        $args = [];
+        return $this->render($template, $args);
+    }
+
+    /**
+     * @Route("/about", name="app_about")
+     */
+    public function about(): Response
+    {
+        $template = 'default/about.html.twig';
+        $args = [];
+        return $this->render($template, $args);
+    }
+
+    /**
+     * @Route("/sitemap", name="app_sitemap")
+     */
+    public function sitemap(): Response
+    {
+        $template = 'default/sitemap.html.twig';
         $args = [];
         return $this->render($template, $args);
     }
